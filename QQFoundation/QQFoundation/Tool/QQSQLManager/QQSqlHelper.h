bac48e2478d97618360cd63856725190fbe7ff5a
wangzs <wangzs@smartdot.com.cn>
2017-12-29 08:31:56 +0800

    秦

---
+++ QQFoundation/QQFoundation/Tool/QQSQLManager/QQSqlHelper.h
@@ -0,0 +1,14 @@
+//
+//  QQSqlHelper.h
+//  QQFoundation
+//
+//  Created by Maybe on 2017/12/28.
+//  Copyright © 2017年 MuYanQin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FMDB.h"
+@interface QQSqlHelper : NSObject
+
+
+@end
---
+++ QQFoundation/QQFoundation/Tool/QQSQLManager/QQSqlHelper.m
@@ -0,0 +1,15 @@
+//
+//  QQSqlHelper.m
+//  QQFoundation
+//
+//  Created by Maybe on 2017/12/28.
+//  Copyright © 2017年 MuYanQin. All rights reserved.
+//
+
+#import "QQSqlHelper.h"
+@interface QQSqlHelper ()
+@property (nonatomic,strong) FMDatabaseQueue *DBQ;
+@end
+@implementation QQSqlHelper
+
+@end
