808a9f59a624b2e3b8eb9013ad37b689236ee81a
wangzs <wangzs@smartdot.com.cn>
2018-02-27 20:07:46 +0800

    秦

---
+++ QQFoundation/QQFoundation/Tool/QQAlertController/DefaultAnimation.h
@@ -0,0 +1,13 @@
+//
+//  DefaultAnimation.h
+//  QQFoundation
+//
+//  Created by Maybe on 2018/1/23.
+//  Copyright © 2018年 MuYanQin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@interface DefaultAnimation : NSObject<UIViewControllerAnimatedTransitioning>
+
+@end
