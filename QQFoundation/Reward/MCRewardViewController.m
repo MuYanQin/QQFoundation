2b522d61250c38c1cc4b191164f252d50e0299be
MuYanQin <1159854187@qq.com>
2018-10-13 11:27:21 +0800

    no message

--- QQFoundation/Reward/MCRewardViewController.m
+++ QQFoundation/Reward/MCRewardViewController.m
@@ -23,24 +23,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.navigationItem.title = @"案例使用2";
-    UILabel * TLabel  = [UILabel getLabel].Qtext(@"秦慕乔").Qframe(CGRectMake(100, 100, 100, 30));
-    [self.view addSubview:TLabel];
-    
-    QQButton *button = [QQButton buttonWithType:UIButtonTypeCustom];
-    button.QtitleFont([UIFont systemFontOfSize:14]).QtitleText(@"测试文字位置").Qframe(CGRectMake(100, 200, 200, 50)).Qtarget(self,@selector(clic:)).QtitleClolor([UIColor redColor]).QbackgroudClolor([UIColor lightGrayColor]).Qimage([UIImage imageNamed:@"tab_launch"]).QInfo(@"info").QtextPosition(Tright).QimageSize(CGSizeMake(50, 50));
-    [self.view addSubview:button];
-    
-    UILabel *leb = [UILabel getLabel].Qtext(@"我是个好人").QtextColor([UIColor redColor]).Qclick(self,@"tttt").Qframe(CGRectMake(100, 300, 100, 30));
-    [self.view addSubview:leb];
-}
-- (void)tttt
-{
-    NSLog(@"===============");
-}
-- (void)clic:(QQButton *)btn;
-{
-    NSLog(@"%@",btn.info);
-    [btn startCountdown];
+
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
