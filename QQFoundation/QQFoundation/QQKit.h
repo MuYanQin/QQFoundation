223f6bb0250633898fc4ebf5ecc73fa2bec9f417
leaduadmin <1159854187@qq.com>
2019-04-16 09:40:57 +0800

    修改文件路径

--- QQFoundation/QQFoundation/QQKit.h
+++ QQFoundation/QQFoundation/QQKit.h
@@ -19,44 +19,44 @@
 #import "QQAppDefine.h"
 #import "MCUserInfo.h"//用户的信息
 ///***********************************************************************************
+///view
+///***********************************************************************************
+#import "MCPickerView.h"
+#import "UIView+MCPopView.h"
+#import "QQImagePicker.h"///照片单选 裁剪框自定义大小
+#import "QQScanVC.h"///
+#import "QQtableView.h"/// 简单的封装数据请求层
+#import "QQTableViewManager.h"//TableView视图管理工具
+#import "QQAlertcontroller.h"///UIAlertConotrooler的快速使用
+#import "MCHoveringView.h"///头部悬浮
+#import "MCVerificationCodeView.h"//验证码view
+#import "MCPageView.h"//类似新闻类app首页侧滑功能
+#import "MCSearchViewController.h"//搜索界面
 
 ///***********************************************************************************
+///core
+///***********************************************************************************
+#import "MCHealthManage.h"
 #import "NSMutableAttributedString+Manage.h"
 #import "NSString+Manage.h"
-#import "NSDate+QQCalculate.h"///获取时间日期的一些方法
-#import "NSString+QQCalculate.h"///字符串的一些操作
-#import "UIImage+Usually.h"///
-#import "UIScrollView+Touch.h"///解决ScrollView 的滑动与touch事件冲突
-#import "UIView+MBProgress.h"
-#import "UIView+QQFrame.h"/// 快速获取View的left、right、top、bottom
-#import "UIView+MCPopView.h"//快捷的弹出视图分类
-#import "UILabel+MCChained.h"//链式语法创建label
-#import "UIButton+MCChained.h"//链式语法创建button
-
-///***********************************************************************************
-
-///***********************************************************************************
-#import "CALayer+XibConfiguration.h"///nib文件设置button的边框
-#import "QQButton.h"///
+#import "QQTool.h"///以下简单的小工具
 #import "QQDevice.h"//获取系统的一些情况
 #import "QQFileManage.h"///获取文件管理  获取地址  创建  等操作
+#import "UIScrollView+Touch.h"///解决ScrollView 的滑动与touch事件冲突
+#import "UIImage+Usually.h"///
+#import "UIView+QQFrame.h"/// 快速获取View的left、right、top、bottom
+#import "NSDate+QQCalculate.h"///获取时间日期的一些方法
+#import "NSString+QQCalculate.h"///字符串的一些操作
+#import "QQButton.h"///
 #import "QQTextField.h"/// 最大限制文字数
 #import "QQTextView.h"/// 有展位文字、最大限制文字数
-
-///***********************************************************************************
-
-///***********************************************************************************
-#import "MCVerificationCodeView.h"//验证码view
+#import "UILabel+MCChained.h"//链式语法创建label
+#import "UIButton+MCChained.h"//链式语法创建button
 #import "MCFactory.h"//工程模式工具
 #import "MCPushMediator.h"//push界面的中介者 解耦合->复杂的push关系
 #import "MCDownloadManager.h"//下载管理类 ->半成品
-#import "MCPageView.h"//类似新闻类app首页侧滑功能
-#import "QQAlertcontroller.h"///UIAlertConotrooler的快速使用
 #import "QQSqlHelper.h"//sql工具
-#import "QQScanVC.h"/// 
 #import "QQNetManager.h"/// 封装的AF下载工具  返回取消、同一接口多次请求返回（无响应）
-#import "QQImagePicker.h"///照片单选 裁剪框自定义大小
-#import "QQtableView.h"/// 简单的封装数据请求层
-#import "QQTableViewManager.h"//TableView视图管理工具
-#import "QQTool.h"///以下简单的小工具 一些文件操作   获取app的一些信息   后期要分离
+#import "NSObject+MB.h"
+
 #endif /* QQKit_h */
