c2cdcee8e57e45e7ff7553a1abe5721dd16f13d9
qinmuqiao <1159854187@qq.com>
2020-08-06 11:32:34 +0800

    1

--- QQFoundation/QQFoundation/View/QQCollectionView/QQCollectionView.h
+++ QQFoundation/QQFoundation/View/QQCollectionView/QQCollectionView.h
@@ -3,14 +3,38 @@
 //  QQFoundation
 //
 //  Created by leaduMac on 2020/6/8.
-//  Copyright © 2020 Yuan er. All rights reserved.
+//  Copyright © 2020 慕纯. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
+@class QQCollectionView;
+@protocol QQCollectionViewRequestDelegate <NSObject>
+@optional
+/**
+ 返回请求到的数据
+ @param QQCollectionView 返回自己
+ @param pullDown   返回bool 表明  YES－－下拉刷新  NO －－－ 上拉记载
+ @param successData         返回的数据
+ */
+- (void)QQCollectionView:(QQCollectionView *)QQCollectionView isPullDown:(BOOL)pullDown successData:(id)successData;
+
+/**
+ 返回网络错误的状态
+
+ @param QQCollectionView self
+ @param error 错误error
+ */
+@optional
+- (void)QQCollectionView:(QQCollectionView *)QQCollectionView requestFailed:(NSError *)error;
+
+@end
 
 @interface QQCollectionView : UICollectionView<UIGestureRecognizerDelegate>
+
+@property (assign,nonatomic) id<QQCollectionViewRequestDelegate> requestDelegate;
+
 
 //配合MCHoveringView使用的属性/
 //**获取tableView偏移量的Block*/
@@ -20,7 +44,21 @@ NS_ASSUME_NONNULL_BEGIN
 /**是否同时响应多个手势 默认NO*/
 @property (nonatomic , assign) BOOL  canResponseMutiGesture;
 
+/*传递controller进来展示loadding状态只能是weak不会引用*/
+@property (weak, nonatomic)UIViewController *tempController;
+
+/**
+ 请求的网址
+ */
 @property (nonatomic , copy) NSString * requestUrl;
+
+/**
+ 请求的参数
+ */
+@property (nonatomic , strong) NSMutableDictionary * requestParam;
+
+- (void)netWorkStart:(NSString *)url param:(NSDictionary *)param vc:(UIViewController *)controller;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- QQFoundation/QQFoundation/View/QQCollectionView/QQCollectionView.m
+++ QQFoundation/QQFoundation/View/QQCollectionView/QQCollectionView.m
@@ -3,13 +3,19 @@
 //  QQFoundation
 //
 //  Created by leaduMac on 2020/6/8.
-//  Copyright © 2020 Yuan er. All rights reserved.
+//  Copyright © 2020 慕纯. All rights reserved.
 //
 
 #import "QQCollectionView.h"
 #import "MJRefresh.h"
-@implementation QQCollectionView
+#import "QQNetManager.h"
+static NSString * const pageIndex = @"pageNum";//获取第几页的根据自己的需求替换
 
+@implementation QQCollectionView
+{
+    /**纪录当前页数*/
+    NSInteger _pageNumber;
+}
 - (instancetype)initWithFrame:(CGRect)frame collectionViewLayout:(UICollectionViewLayout *)layout
 {
     if (self = [super initWithFrame:frame collectionViewLayout:layout]) {
@@ -30,6 +36,15 @@
         self.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
     }
 }
+- (void)netWorkStart:(NSString *)url param:(NSDictionary *)param vc:(UIViewController *)controller{
+    _requestUrl = url;
+    _tempController = controller;
+    _requestParam = param.mutableCopy;
+    if ([param.allKeys containsObject:pageIndex]) {
+        self.mj_footer = [MJRefreshBackStateFooter footerWithRefreshingTarget:self refreshingAction:@selector(footerRefresh)];
+    }
+    [self.mj_header beginRefreshing];
+}
 - (void)requestData
 {
     if (self.requestUrl.length ==0) {
@@ -37,7 +52,54 @@
         [self.mj_header endRefreshing];
         return;
     }
+    if ([_requestParam.allKeys containsObject:pageIndex]) {
+        [self changeIndexWithStatus:1];
+    }
+    [self setUpNetWorkParamters:_requestParam isPullDown:YES];
+}
 
+- (void)footerRefresh
+{
+    [self changeIndexWithStatus:2];
+    [self setUpNetWorkParamters:_requestParam isPullDown:NO];
+}
+- (void)changeIndexWithStatus:(NSInteger)Status//1  下拉  2上拉  3减一
+{
+    _pageNumber = [_requestParam[pageIndex] integerValue];
+    if (Status == 1) {
+        _pageNumber = 1;
+    }else if (Status == 2){
+        _pageNumber ++;
+    }else{
+        _pageNumber --;
+    }
+    [_requestParam setValue:@"10" forKey:@"pageSize"];
+    [_requestParam setObject:[NSNumber numberWithInteger:_pageNumber] forKey:pageIndex];
+}
+//**请求方法*/
+- (void)setUpNetWorkParamters:(NSDictionary *)paramters isPullDown:(BOOL)isPullDown
+{
+    
+    [[QQNetManager Instance] RTSPostJsonWith:_requestUrl parameters:paramters from:_tempController successs:^(id responseObject) {
+        if ([self.requestDelegate respondsToSelector:@selector(QQCollectionView:isPullDown:successData:)]) {
+            [self.requestDelegate QQCollectionView:self isPullDown:isPullDown successData:responseObject[@"data"]];
+        }
+        [self endRefrseh];
+    } failed:^(NSError *error) {
+        if ([self.requestDelegate respondsToSelector:@selector(QQCollectionView:requestFailed:)]) {
+            [self.requestDelegate QQCollectionView:self requestFailed:error];
+        }
+        [self endRefrseh];
+        if (!isPullDown) {
+            [self changeIndexWithStatus:3];
+        }
+    }];
+
+}
+- (void)endRefrseh
+{
+    [self.mj_footer endRefreshing];
+    [self.mj_header endRefreshing];
 }
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
 {
